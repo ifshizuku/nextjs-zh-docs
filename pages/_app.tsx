bdadacde66a8485860df4f10232c90846d26bf86
Kawasaki Shizuku <starsmoe@outlook.com>
2022-07-20 11:47:48 +0800

    chore: add dynamic site favicon
    Add dynamic site favicon (hooks)
    Sync official repo: basic-features/eslint

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,30 @@
 import "../styles/globals.css"
 import "nextra-theme-docs/style.css"
 
+import Head from "next/head"
 import React from "react"
+import useSystemTheme from "use-system-theme"
 
 export default function Nextra({ Component, pageProps }) {
+    const systemTheme = useSystemTheme()
     const getLayout = Component.getLayout || ((page) => page)
-    return getLayout(<Component {...pageProps} />)
+    return getLayout(
+        <>
+            <Head>
+                <>
+                    <link
+                        rel="icon"
+                        type="image/png"
+                        sizes="256x256"
+                        href={`/favicons/nextjs-icon-${systemTheme}.png`}
+                    />
+                    <link
+                        rel="shortcut icon"
+                        href={`/favicons/nextjs-icon-${systemTheme}.png`}
+                    />
+                </>
+            </Head>
+            <Component {...pageProps} />
+        </>
+    )
 }
